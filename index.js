40fb495fceacfff8f010b012e25004abe7a712f7
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-05-12 19:39:05 -0300

    Add remove listener

--- index.js
+++ index.js
@@ -8,26 +8,25 @@ const _subscriptions = new Map();
 
 const addEventListener = (event, handler) => {
   const mappedEvent = event;
-  if (mappedEvent) {
-    let listener;
-    if (event === "adFailedToLoad") {
-      listener = eventEmitter.addListener(mappedEvent, (error) =>
-        handler(createErrorFromErrorData(error))
-      );
-    } else {
-      listener = eventEmitter.addListener(mappedEvent, handler);
-    }
-    _subscriptions.set(handler, listener);
-    return {
-      remove: () => removeEventListener(event, handler),
-    };
-  } else {
-    // eslint-disable-next-line no-console
-    console.warn(`Trying to subscribe to unknown event: "${event}"`);
-    return {
-      remove: () => {},
-    };
+
+  let listener;
+
+  listener = eventEmitter.addListener(mappedEvent, handler);
+
+  _subscriptions.set(handler, listener);
+
+  return {
+    remove: () => removeEventListener(event, handler),
+  };
+};
+
+const removeEventListener = (type, handler) => {
+  const listener = _subscriptions.get(handler);
+  if (!listener) {
+    return;
   }
+  listener.remove();
+  _subscriptions.delete(handler);
 };
 
 export default {
